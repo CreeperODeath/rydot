516fd978e2fbd4187b175aa92c941348ce5e8e96
Roach11345 <122185507+Roach11345@users.noreply.github.com>
2023-01-08 21:35:54 -0600

    firing on a timer

--- Player.gd
+++ Player.gd
@@ -3,9 +3,11 @@ extends KinematicBody2D
 export var move_speed = 300.0
 onready var BulletInstance = preload("res://Bullet.tscn")
 onready var player_sprite = $AnimatedSprite
+onready var WeaponTimer1 = $WeaponTimer1
 var velocity = Vector2()
 
 var facing_left = true
+var firing = false
 
 func _ready():
 	pass
@@ -18,26 +20,29 @@ func _process(_delta):
 #-------------------Shooty code--------------------
 	if Input.is_action_just_pressed("exit"):
 		get_tree().quit()
-	if Input.is_action_just_pressed("fire_right"):
+	if Input.is_action_pressed("fire_right") or Input.is_action_pressed("fire_left") or Input.is_action_pressed("fire_up") or Input.is_action_pressed("fire_down"):
+		firing = true
+	else:
+		firing = false
+	if firing == true and WeaponTimer1.is_stopped():
+		WeaponTimer1.start()
 		var Bullet = BulletInstance.instance()
-		Bullet.position = self.position
-		Bullet.direction = Vector2(1, 0)
-		get_parent().add_child(Bullet)
-	if Input.is_action_just_pressed("fire_left"):
-		var Bullet = BulletInstance.instance()
-		Bullet.position = self.position
-		Bullet.direction = Vector2(-1, 0)
-		get_parent().add_child(Bullet)
-	if Input.is_action_just_pressed("fire_up"):
-		var Bullet = BulletInstance.instance()
-		Bullet.position = self.position
-		Bullet.direction = Vector2(0, -1)
-		get_parent().add_child(Bullet)
-	if Input.is_action_just_pressed("fire_down"):
-		var Bullet = BulletInstance.instance()
-		Bullet.position = self.position
-		Bullet.direction = Vector2(0, 1)
-		get_parent().add_child(Bullet)
+		if Input.is_action_pressed("fire_right"):
+			Bullet.position = self.position
+			Bullet.direction = Vector2(1, 0)
+			get_parent().add_child(Bullet)
+		if Input.is_action_pressed("fire_left"):
+			Bullet.position = self.position
+			Bullet.direction = Vector2(-1, 0)
+			get_parent().add_child(Bullet)
+		if Input.is_action_pressed("fire_up"):
+			Bullet.position = self.position
+			Bullet.direction = Vector2(0, -1)
+			get_parent().add_child(Bullet)
+		if Input.is_action_pressed("fire_down"):
+			Bullet.position = self.position
+			Bullet.direction = Vector2(0, 1)
+			get_parent().add_child(Bullet)
 
 func _physics_process(_delta):
 	#----------------Movey Code-----------------
