39c5aeed8a793b7514c8079fc48787dcc66b812d
CreeperODeath <63520918+CreeperODeath@users.noreply.github.com>
2023-01-10 17:33:14 -0600

    clean junk code

--- World.gd
+++ World.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
-onready var MapInstance = preload("res://maps/test map.tscn")
-onready var PlayerInstance = preload("res://Player.tscn")
-onready var start_room = preload("res://maps/Rooms/start_room.tscn")
-onready var left_node = $start_room/room_connectors/left
+
 
 
 var time = 600
@@ -13,9 +10,7 @@ func _on_Timer_timeout():
 
 
 func _ready():
-	var Player = PlayerInstance.instance()
-	var Map = MapInstance.instance()
-
+	pass
 
 func _process(delta):
 	pass
