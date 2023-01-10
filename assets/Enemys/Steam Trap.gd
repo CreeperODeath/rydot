38c8c690aef02fc7520fd4346fe2928579790e07
Roach11345 <122185507+Roach11345@users.noreply.github.com>
2023-01-09 18:54:21 -0600

    Steam Trap

---
+++ assets/Enemys/Steam Trap.gd
@@ -0,0 +1,21 @@
+extends StaticBody2D
+
+onready var animation = $AnimatedSprite
+
+
+
+func _ready():
+	pass 
+
+
+func _process(delta):
+	pass
+
+func _on_Search_body_entered(area):
+	if area.is_in_group("player") and animation.animation != ("Loop"):
+		animation.play("Open")
+
+
+func _on_AnimatedSprite_animation_finished():
+	if animation.animation == ("Open"):
+		animation.play("Loop")
