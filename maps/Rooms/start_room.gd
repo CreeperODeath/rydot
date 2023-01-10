160d48d0df046512bfa69103ab2deb876b07b9b7
CreeperODeath <63520918+CreeperODeath@users.noreply.github.com>
2023-01-09 18:39:02 -0600

    room_variables

--- maps/Rooms/start_room.gd
+++ maps/Rooms/start_room.gd
@@ -4,6 +4,8 @@ onready var room_var = room_variables.new()
 
 
 
+
+
 #----------left room var----------
 onready var left_room = load(room_var.top_rooms[0])
 onready var left_node = $room_connectors/left
@@ -15,7 +17,24 @@ onready var right_room = load(room_var.top_rooms[0])
 onready var right_node = $room_connectors/right
 var right_spawned = false
 
+#---------top room var-----------
+onready var top_room = load(room_var.top_rooms[0])
+onready var top_node = $room_connectors/right
+var top_spawned = false
+
+#---------bottom room var-----------
+onready var bottom_room = load(room_var.top_rooms[0])
+onready var bottom_node = $room_connectors/right
+var bottom_spawned = false
+
+
+
+#-------individual room stats--------
 export var room_id = ""
+export var left_opening = false
+export var right_opening = false
+export var top_opening = false
+export var bottom_opening = false
 
 func _physics_process(delta):
 	if left_spawned == false:
@@ -38,3 +57,19 @@ func spawn_room_right():
 	room_instence.position.x = right_node.position.x - 9
 	room_instence.position.y = position.y
 	right_spawned = true
+	
+func spawn_room_top():
+	var room_instence = right_room.instance()
+	get_parent().add_child(room_instence)
+	room_instence.position.x = right_node.position.x - 9
+	room_instence.position.y = position.y
+	right_spawned = true
+	
+	
+func spawn_room_bottom():
+	var room_instence = right_room.instance()
+	get_parent().add_child(room_instence)
+	room_instence.position.x = right_node.position.x - 9
+	room_instence.position.y = position.y
+	right_spawned = true
+
