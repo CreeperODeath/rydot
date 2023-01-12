92165366c8100b848d1c00d0a3326416088e3b37
CreeperODeath <63520918+CreeperODeath@users.noreply.github.com>
2023-01-12 15:41:28 -0600

    door close go brrrr

--- maps/Rooms/fish_boss_room.gd
+++ maps/Rooms/fish_boss_room.gd
@@ -4,6 +4,8 @@ extends TileMap
 var doors_closed = false
 var player_entered = false
 onready var bottom_door_node = $room_connectors/bottom
+onready var left_node = $room_connectors/left
+onready var right_node = $room_connectors/right
 
 
 
@@ -19,6 +21,11 @@ func close_doors():
 	set_cell(1,8,7)
 	set_cell(1,9,7)
 	set_cell(1,10,7)
+	#right
+	set_cell(31,8,9)
+	set_cell(31,9,9)
+	set_cell(31,10,9)
+	
 	
 	
 	update_dirty_quadrants()
@@ -35,5 +42,9 @@ func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
 		var player_pos = body.get_global_position()
 		if player_pos.y >= bottom_door_node.global_position.y:
-			body.set_position(Vector2(player_pos.x, player_pos.y - 9))
+			body.set_global_position(Vector2(player_pos.x, player_pos.y - 9))
+		elif player_pos.x < left_node.global_position.x:
+			body.set_global_position(Vector2(left_node.global_position.x + 6, player_pos.y))
+		elif player_pos.x >= right_node.global_position.x:
+			body.set_global_position(Vector2(right_node.global_position.x - 5, player_pos.y))
 		close_doors()
