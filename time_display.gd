064f08d18d005f6340735b1c55a28277024e33a7
Roach11345 <122185507+Roach11345@users.noreply.github.com>
2023-01-15 12:15:56 -0600

    Low Time Alert

--- time_display.gd
+++ time_display.gd
@@ -4,6 +4,8 @@ extends RichTextLabel
 var time
 var time_display_min
 var time_display_sec
+onready var SoundTimer = $SoundTimer
+onready var LowTime = $LowTime
 onready var world = get_node("/root/World")
 func _process(_delta):
 	if is_instance_valid(world):
@@ -13,4 +15,7 @@ func _process(_delta):
 		time_display_sec = time%60
 		if time_display_sec <10:
 			time_display_sec = "0" + str(time_display_sec)
+			if SoundTimer.is_stopped():
+				SoundTimer.start()
+				LowTime.play()
 		set_text("Time " + str(time_display_min) + ":" + str(time_display_sec))
