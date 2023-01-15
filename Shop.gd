c13044fae3b16c267c7a2a8a2c52de30af2bb61d
Roach11345 <122185507+Roach11345@users.noreply.github.com>
2023-01-15 01:57:32 -0600

    Reviewed Pre-existing rooms, eradicated Slime Enemy

--- Shop.gd
+++ Shop.gd
@@ -4,6 +4,9 @@ func _on_BuyZone_body_entered(body):
 	if body == get_node("Player"):
 		get_tree().change_scene("res://Shop_Menu.tscn")
 
+func _on_PortalZone_body_entered(body):
+	if body == get_node("Player"):
+		get_tree().change_scene("res://World.tscn")
+
 func _ready():
 	pass
-
